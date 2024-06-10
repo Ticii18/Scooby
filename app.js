1c50395588c4975657fdfd33cb0aa715d879070b
unknown <ticianovera22@gmail.com>
2024-06-10 14:55:22 -0300

    se acoplo las pantallas al js

--- app.js
+++ app.js
@@ -1,89 +1,58 @@
-const express  = require("express");
-const app = express();
+const express = require("express");
 const path = require("path");
 const { newConex } = require("./db");
 
+const app = express();
 app.use(express.json());
-const PORT = 4000
+app.use(express.static(path.join(__dirname, 'public'))); // Carpeta para archivos estáticos
 
-// obtener los libros
+const PORT = 4000;
+
+// Rutas para libros (tu código existente)
 app.get("/books", async (req, res) => {
-
-    const conex = await newConex()
-
-    const results =  await conex.query("SELECT * FROM books")
-
-    // res.send("obtener todos los libros")
-    // console.log(results);
-
+    const conex = await newConex();
+    const results = await conex.query("SELECT * FROM books");
     res.json(results[0]);
-    conex.end()
-})
-
-// editar un libro especifico
+    conex.end();
+});
 
 app.put("/books/:id", async (req, res) => {
-
-    const conex = await newConex()
-    const id = req.params.id
-    const {title, Author} = req.body 
-
-    conex.query(`UPDATE books SET title =?, author =? WHERE id =?`, [title, Author, id])
-    
-    res.send("editar un book") 
-    conex.end()
-})
-
-// eliminar un libro
+    const conex = await newConex();
+    const id = req.params.id;
+    const { title, author } = req.body;
+    conex.query(`UPDATE books SET title = ?, author = ? WHERE id = ?`, [title, author, id]);
+    res.send("Libro actualizado");
+    conex.end();
+});
 
 app.delete("/books/:id", async (req, res) => {
-
-    const conex = await newConex()
-    const id = req.params.id
-
-    conex.query(`DELETE FROM books WHERE id =?`, id)
-    
-    res.send("eliminar un book")
-    conex.end()
-})
-
-
-//obtener un solo libro
+    const conex = await newConex();
+    const id = req.params.id;
+    conex.query(`DELETE FROM books WHERE id = ?`, [id]);
+    res.send("Libro eliminado");
+    conex.end();
+});
 
 app.get("/books/:id", async (req, res) => {
-
-    const conex = await newConex()
-    const id = req.params.id
-
-    const results =  await conex.query("SELECT * FROM books WHERE id =?", id)
-
-    // res.send("obtener todos los libros")
-    // console.log(results);
-
+    const conex = await newConex();
+    const id = req.params.id;
+    const results = await conex.query("SELECT * FROM books WHERE id = ?", [id]);
     res.json(results[0]);
-    conex.end()
-})
-
-
-//creaar un nuevo libro
-app.post("/books",async (req, res) => {
-    
-    console.log(req.body);
-    
-    const conex = await newConex()
-    const {title, author} = req.body 
-    conex.query(`INSERT INTO books (title, author) VALUES ( ? , ? )`, [title, author])
-    
-    
-    res.send("crear un book")
-    conex.end()
-
-
-
-})
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html')); // Adjust the path if necessary
+    conex.end();
 });
+
+app.post("/books", async (req, res) => {
+    const conex = await newConex();
+    const { title, author } = req.body;
+    conex.query(`INSERT INTO books (title, author) VALUES (?, ?)`, [title, author]);
+    res.send("Libro creado");
+    conex.end();
+});
+
+// Servir el archivo HTML principal
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html')); // Asegúrate de que esta ruta es correcta
+});
+
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}/index.html`));
