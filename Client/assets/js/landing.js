114e1a1f0ab5c8c73e0c20544b5a86bdd14f6cc7
Leonardo <leoonardoogzz@gmail.com>
2024-06-23 19:53:17 -0300

    Se agregaron botones en el index

--- Client/assets/js/landing.js
+++ Client/assets/js/landing.js
@@ -12,22 +12,51 @@ const pintarPublicaciones = (data) => {
 
         // Vamos agregando divs con la información de cada publicación.
         tbody.innerHTML += `
-            <div class="post">
-                <div class="post-header">
-                    <div class="post-header-info">
-                        <h3>${publicacion.titulo}</h3>
-                    </div>
-                </div>
-                <div class="post-content">
-                    <p>${publicacion.contenido}</p>
-                    <img src="${publicacion.imagen_url}" alt="${publicacion.titulo}" />
-                </div>
-                <div class="post-actions">
-                    <button>Me gusta</button>
-                    <button>Comentar</button>
-                    <button>Compartir</button>
-                </div>
+           <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Publicación</title>
+    <style>
+        .post-header-info button {
+    margin-left:380px;
+    background-color: rgb(230, 70, 82);
+    color: white;
+    border: none;
+    border-radius: 50%;
+    width: 30px;
+    height: 30px;
+    cursor: pointer;
+}
+   
+    </style>
+</head>
+
+<body>
+    <div class="post">
+        <div class="post-header">
+            <div class="post-header-info">
+                <h3>${publicacion.titulo}</h3>
+                <button>🗑️</button>
             </div>
+        </div>
+        <div class="post-content">
+            <p>${publicacion.contenido}</p>
+            <img src="${publicacion.imagen_url}" alt="${publicacion.titulo}" />
+        </div>
+        <div class="post-actions">
+            <button>Me gusta</button>
+            <button>Comentar</button>
+            <button>Compartir</button>
+        </div>
+    </div>
+</body>
+
+</html>
+
         `;
     });
 };
